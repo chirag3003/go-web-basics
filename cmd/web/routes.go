8bf23e94cd31ca191e664b660c370986c58a6a77
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2021-12-23 11:48:04 +0530

    NoSurf middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,11 +9,8 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	//mux := pat.New()
-	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	mux := chi.NewRouter()
-	mux.Use(middleware.Recoverer, middleware.Logger, WriteToConsole)
+	mux.Use(middleware.Recoverer, middleware.Logger, WriteToConsole, NoSurf)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
