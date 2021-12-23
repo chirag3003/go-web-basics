d6803cf1013d7a962f780eb51eb25178a294b6bf
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2021-12-23 11:20:32 +0530

    started with middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
 	"github.com/chirag3003/go-web/pkg/config"
 	"github.com/chirag3003/go-web/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	//mux := pat.New()
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer, middleware.Logger, WriteToConsole)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
