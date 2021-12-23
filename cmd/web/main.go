581113a0208e4645292770ad8ac0869137187fd2
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2021-12-23 12:25:30 +0530

    setup sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,28 +2,43 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/chirag3003/go-web/pkg/config"
 	"github.com/chirag3003/go-web/pkg/handlers"
 	"github.com/chirag3003/go-web/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const PORT = ":3000"
 
-func main() {
-	var app config.AppConfig
+var app config.AppConfig
+var session *scs.SessionManager
 
+func main() {
+	app.InProduction = false //change it to true when in production
+
+	//session configs
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
+	//html template config
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
 	app.TemplateCache = tc
 	app.UseCache = false
-	repo := handlers.NewRepo(&app)
-
-	handlers.NewHandlers(repo)
 	render.NewTemplate(&app)
+
+	//HTTP Handlers config
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
 
 	//http.HandleFunc("/", handlers.Repo.Home)
 	//http.HandleFunc("/about", handlers.Repo.About)
